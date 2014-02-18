7295eaf16589773deb2acbb70890bf9eb8480d40
Andy Provan <ap12750@my.bristol.ac.uk>
2014-02-18 14:22:47 +0000

    Minor change to allow for compilation for pre Android 3.0 devices

--- App/src/main/java/com/lemonslice/brainslice/MainActivity.java
+++ App/src/main/java/com/lemonslice/brainslice/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lemonslice.brainslice;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
@@ -293,26 +294,25 @@ public class MainActivity extends Activity {
     public void onWindowFocusChanged(boolean hasFocus)
     {
         // Immersive mode is only supported in Android KitKat and above
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-        {
             super.onWindowFocusChanged(hasFocus);
             if (hasFocus)
-            {
                 hideSystemBars();
-            }
-        }
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     private void hideSystemBars()
     {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        );
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
     }
 
     class MyRenderer implements GLSurfaceView.Renderer {

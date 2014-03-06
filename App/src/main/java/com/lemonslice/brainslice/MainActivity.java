2f1e64a556b7529d11159bf3cf6e3ee4a0714b2f
James Burnside <mail@jamesburnside.com>
2014-03-06 20:31:51 +0000

    Made a translucent screen to be replaced with a calibration instruction screen

--- App/src/main/java/com/lemonslice/brainslice/MainActivity.java
+++ App/src/main/java/com/lemonslice/brainslice/MainActivity.java
@@ -87,8 +87,10 @@ public class MainActivity extends Activity {
         LoadingScreen.setRenderer(renderer);
         LoadingScreen.showLoadingScreen();
 
-        Labels.setFrameLayout((FrameLayout) findViewById(R.id.overlay_layout));
         Labels.setContext(this);
+        Labels.setFrameLayout(overlayingFrame);
+        OverlayScreen.setContext(this);
+        OverlayScreen.setFrameLayout(overlayingFrame);
 
         super.onCreate(savedInstanceState);
         mGLView = (GLSurfaceView)findViewById(R.id.openGlView);
@@ -137,6 +139,7 @@ public class MainActivity extends Activity {
             {
                 baseController.stop();
                 BrainModel.smoothRotateToFront(200);
+                OverlayScreen.showScreen();
             }
         });
 

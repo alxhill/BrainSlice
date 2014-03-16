bcd429a7ab6795bc2a837ced8ba59d391d972fb6
Alexander Hill <me@alxhill.com>
2014-03-16 20:21:23 +0000

    Fixed calibrate button bug

--- App/src/main/java/com/lemonslice/brainslice/OverlayScreen.java
+++ App/src/main/java/com/lemonslice/brainslice/OverlayScreen.java
@@ -41,10 +41,16 @@ public class OverlayScreen
 
         calibrateScreen.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Event.trigger("tap:calibrate");
+            public void onClick(View v) {
                 hideScreen();
+            }
+        });
+
+        Button calibrateBtn = (Button)calibrateScreen.findViewById(R.id.calibrateOverlayBtn);
+        calibrateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Event.trigger("tap:calibrate");
             }
         });
 

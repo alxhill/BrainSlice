a49e21124560ccaf7ffe6e5f231da09649b4e55d
20k <the_berronator@hotmail.co.uk>
2014-03-17 05:13:53 +0000

    Fixed lables

--- App/src/main/res/raw/fragmentshader_spheres.glsl
+++ App/src/main/res/raw/fragmentshader_spheres.glsl
@@ -8,7 +8,7 @@ void main ()
 	float dist = length(gl_FragCoord.xy - spherePos.xy);
 	float att = clamp(1.0 - 0.03 * dist, 0.0, 1.0);
 	if(isSelected==1)
-		gl_FragColor = vec4(0.0, att, 0.0, 0.0);
+		gl_FragColor = vec4(0.0, att, 0.0, 0.5);
 	else
-		gl_FragColor = vec4(att, att, att, 0.0);
+		gl_FragColor = vec4(att, att, att, 0.5);
 }
